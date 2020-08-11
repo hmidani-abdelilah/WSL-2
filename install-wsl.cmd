f83e4119d614e640ab24e066eef34c616c2ed119
Hmidani Abdelilah <hmidaniabdelilah92@gmail.com>
2020-08-11 15:10:00 +0100

    set-default-version 2 WSL

--- install-wsl.cmd
+++ install-wsl.cmd
@@ -1,5 +1,13 @@
 @ECHO OFF
 SET RUNSTART=%date% @ %time%
 REM ## Enable WSL-2
+
 POWERSHELL.EXE -command dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 POWERSHELL.EXE -command dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+
+ECHO plase reboot the machine and run the script again or  
+PAUSE
+
+REM ## set-default-version 2 WSL
+
+POWERSHELL.EXE -command wsl --set-default-version 2
