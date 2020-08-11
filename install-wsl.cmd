727e3787d94b86f221f7c1e2eca9f30fe96385c4
Hmidani Abdelilah <hmidaniabdelilah92@gmail.com>
2020-08-11 15:20:15 +0100

    Get install names and port numbers

--- install-wsl.cmd
+++ install-wsl.cmd
@@ -23,5 +23,9 @@ msiexec.exe /i wsl.msi  /quiet /norestart  /L c:\install-log.txt
 ECHO the log file in: c:\install-log.txt
 
 REM ## delete the package
-
 DEL wsl.msi
+
+REM ## Get install names and port numbers
+ECHO WSL for Ubuntu 20.04  
+SET DISTRO=ubuntu2004& SET /p DISTRO=Enter a unique name for the distro or hit Enter to use default [WSL-2]: 
+SET RDPPRT=3390& SET /p RDPPRT=Enter port number for xRDP traffic or hit Enter to use default [3390]:
