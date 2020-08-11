90e6eaea6fae953992c0d801c43b894a947325c9
Hmidani Abdelilah <hmidaniabdelilah92@gmail.com>
2020-08-11 16:18:02 +0100

    download ubuntu 20.04

--- install-wsl.cmd
+++ install-wsl.cmd
@@ -4,12 +4,10 @@ REM ## Enable WSL-2
 
 POWERSHELL.EXE -command dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 POWERSHELL.EXE -command dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
 ECHO plase reboot the machine and run the script again or  
 PAUSE
 
 REM ## set-default-version 2 WSL
-
 POWERSHELL.EXE -command wsl --set-default-version 2
 
 REM ## copy package 
@@ -19,8 +17,8 @@ REM ## download kernel linux and install
 ECHO Downloading Kernel for WSL (or using local copy if available)
 IF NOT EXIST C:\Windows\system32\wsl.msi POWERSHELL.EXE -command Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi  -OutFile wsl.msi
 ECHO install kernel ....
-REM ## install the kernel
 
+REM ## install the kernel
 msiexec.exe /i wsl.msi  /quiet /norestart  /L c:\install-log.txt 
 ECHO the log file in: c:\install-log.txt
 
@@ -31,3 +29,9 @@ REM ## Get install names and port numbers
 ECHO WSL for Ubuntu 20.04  
 SET DISTRO=ubuntu2004& SET /p DISTRO=Enter a unique name for the distro or hit Enter to use default [WSL-2]: 
 SET RDPPRT=3390& SET /p RDPPRT=Enter port number for xRDP traffic or hit Enter to use default [3390]:
+
+REM ## Download ubuntu
+SET DISTROFULL=C:\Windows\system32\
+ECHO WSL-2 (%DISTRO%) To be installed in: %DISTROFULL%
+ECHO Downloading Ubuntu 20.04 for WSL (or using local copy if available)
+IF NOT EXIST C:\Windows\system32\%DISTRO%.appx POWERSHELL.EXE -Command Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile %DISTRO%.appx -UseBasicParsing
